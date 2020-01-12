6093681335b5df7bd632b6171988f1bc53cce257
Nicholas Ruppel <npruppel@gmail.com>
2020-01-12 16:18:16 +0900

    work with camera

--- HorrorTestGround/Assets/Scripts/DoorScript01.cs
+++ HorrorTestGround/Assets/Scripts/DoorScript01.cs
@@ -5,25 +5,26 @@ using UnityEngine;
 public class DoorScript01 : MonoBehaviour
 {
     bool b1 = true;
+
     float a = 0f;
     float fadespeed = 0.1f;
     public Transform playerPos;
 
 
 
-	private void Update()
-	{
+    private void Update()
+    {
         Debug.Log(b1);
         Debug.Log(a);
-	}
+    }
 
-	void OnTriggerStay(Collider other)
+    void OnTriggerStay(Collider other)
     {
         if (Input.GetMouseButtonDown(0) && b1 == true)
         {
             playerPos.Translate(Vector3.forward);
             {
-                
+
             }
         }
         /*if(Input.GetMouseButtonDown(1) && b1 == false){
@@ -31,7 +32,7 @@ public class DoorScript01 : MonoBehaviour
             b1 = true;
         }*/
 
-        }
+    }
 
 
 
